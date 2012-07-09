7338854ad47c4d51df8f73b1cc4e2de4e542920b
Lepidosteus <lepidosteus at gmail>
2012-07-09 22:02:16 +0200

    Add license and README Bit of bugfix in dip code

--- dip/application/tools/ci_core.php
+++ dip/application/tools/ci_core.php
@@ -3,13 +3,14 @@ function get_instance()
 {
   static $_has_initialized = false;
 
+  // if we already did the fake init, we can let CI handle the get_instance() call itself
   if ($_has_initialized) {
     return CI_Controller::get_instance();
   }
 
   define('NO_CI_INIT', true);
 
-  require(dirname(__FILE__)'/../../index.php'); // make sure this line points to where your index.php file is
+  require(dirname(__FILE__).'/../../index.php'); // make sure this line points to where your index.php file is
 
   require(BASEPATH.'core/Common.php');
 
@@ -23,7 +24,14 @@ function get_instance()
     get_config(array('subclass_prefix' => $assign_to_config['subclass_prefix']));
   }
 
+  // we need to fake our script name before loading ::Config, in case our base_url config setting isn't set
+  $script_name_bak = $_SERVER['SCRIPT_NAME'];
+  $_SERVER['SCRIPT_NAME'] = '/';
+
   $CFG =& load_class('Config', 'core');
+
+  // done, reset the proper value now
+  $_SERVER['SCRIPT_NAME'] = $script_name_bak;
 
   if (isset($assign_to_config)) {
     $CFG->_assign_to_config($assign_to_config);
